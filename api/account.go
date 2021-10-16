b1007493222de408f62a273d4a6cf4d8d13d3b48
Lucas Barroso <55262061+lucasbarroso23@users.noreply.github.com>
2021-10-15 23:32:24 -0300

    delete account route

--- api/account.go
+++ api/account.go
@@ -93,3 +93,27 @@ func (server *Server) listAccount(ctx *gin.Context) {
 
 // TODO update account
 // TODO delete account
+
+type deleteAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) deleteAccount(ctx *gin.Context) {
+	var req deleteAccountRequest
+
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	err := server.store.DeleteAccount(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+
+	ctx.JSON(http.StatusOK, "Account successfully deleted")
+}
