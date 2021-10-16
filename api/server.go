4d8a9521a99af394cbc19a164772af44483a0d64
Lucas Barroso <55262061+lucasbarroso23@users.noreply.github.com>
2021-10-15 21:23:14 -0300

    api rest using gin

---
+++ api/server.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	db "backend-master-class/db/sqlc"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Server serves HTTP requests for our banking service
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	server.router = router
+	return server
+}
+
+// Start runs the HTTP server on a specific address
+func (server *Server) Start(adress string) error {
+	return server.router.Run(adress)
+}
+
+// errorResponse return a json error response
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"backend-master-class/api"
+	db "backend-master-class/db/sqlc"
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq" // postgres go driver
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:password@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
 
 func main() {
-	fmt.Print("hello backend master class")
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
 }
